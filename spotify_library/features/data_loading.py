7ba025b0ce676ed745d1896bac74119e836d6d03
Vanessa Kromm <vanessa.kromm@web.de>
2023-11-19 10:57:13 +0100

    added description into the function

--- spotify_library/features/data_loading.py
+++ spotify_library/features/data_loading.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
 # Function to load the data
-def loaddata(x):
-    dfraw = pd.read_csv(x)
+def loaddata(path: str) -> pd.DataFrame:
+    """
+    Load data from a CSV file into a Pandas DataFrame.
+
+    Args:
+    - path (str): The file path to the CSV file.
+
+    Returns:
+    - pd.DataFrame: A Pandas DataFrame containing the data from the CSV file. 
+    """    
+    
+    dfraw = pd.read_csv(path)
     df = pd.DataFrame(dfraw)
     return df

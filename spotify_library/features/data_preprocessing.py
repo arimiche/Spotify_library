8c669fb17b6276a22ea4ee2319b4cbfcbacf1cf7
baksben <tatiana.bakwenye@bse.eu>
2023-11-21 22:50:39 +0100

    Added missing values class

--- spotify_library/features/data_preprocessing.py
+++ spotify_library/features/data_preprocessing.py
@@ -16,6 +16,10 @@ class data_preprocessing(metaclass=ABCMeta):
         def transform_columns(self):
             return NotImplementedError
         
+        @abstractmethod
+        def missing_values_table(self):
+             return NotImplementedError
+        
     
 class drop(data_preprocessing):
     """
@@ -83,3 +87,45 @@ class transform_columns(data_preprocessing):
         self.data.drop(self.columns, axis=1, inplace=True)
 
         return self.data
+
+
+class missing_values(data_preprocessing):
+    """
+    Class to work with missing values
+        
+    Args:
+        data (pd.DataFrame): The DataFrame to be processed       
+    """
+    def __init__(self, df):
+        self.df = df
+        
+    def missing_values_table(self, df):
+                """
+        Function to to create table of missing values
+
+        Returns:
+            pd.DataFrame: return the dataframe with missing information
+        """
+        # Total missing values
+                mis_val = self.df.isnull().sum()
+        
+        # Percentage of missing values
+                mis_val_percent = 100 * self.df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+                mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+                mis_val_table_ren_columns = mis_val_table.rename(
+                columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+                mis_val_table_ren_columns = mis_val_table_ren_columns[
+                mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+
+        # Print some summary information
+                print ("Dataframe has " + str(df.shape[1]) + " columns.\n"
+                       "There are " + str(mis_val_table_ren_columns.shape[0]) + " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+                return mis_val_table_ren_columns

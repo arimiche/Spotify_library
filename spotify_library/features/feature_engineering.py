2302828ffb19c2e314c9503ea12cc6b0fdc42476
Vanessa Kromm <vanessa.kromm@web.de>
2023-11-23 16:00:12 +0100

    deleted function for columns including given words, because we have a function that first finds the most relevant words

--- spotify_library/features/feature_engineering.py
+++ spotify_library/features/feature_engineering.py
@@ -2,29 +2,6 @@ import pandas as pd
 from nltk.tokenize import word_tokenize
 from collections import Counter
 
-def create_binary_columns_for_keywords(df: pd.DataFrame, column:str) -> pd.DataFrame:
-    """
-    Create binary columns in a DataFrame based on the presence of given keywords in a specified column
-
-    Args:
-        df (pd.DataFrame): The DataFrame to be processed
-        column (str): The column containing text data for keyword analysis
-
-    Returns:
-        pd.DataFrame: The DataFrame with added binary columns indicating the presence of specific keywords
-    """
-    # Check if the specified column exists in the DataFrame
-    if column not in df.columns:
-        raise ValueError(f"Column '{column}' not found in the DataFrame.")
-
-    # Create binary columns based on specific words
-    df['contains_love'] = df[column].str.contains('love', case=False).astype(int)
-    df['contains_dance'] = df[column].str.contains('dance|dancing', case=False, regex=True).astype(int)
-    df['contains_I_you_we'] = df[column].str.contains(r'\bI\b|\byou\b|\bwe\b', case=False, regex=True).astype(int)
-
-
-    return df
-
 
 def add_party_music_column(df: pd.DataFrame) -> pd.DataFrame:
     """

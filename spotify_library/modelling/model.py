fe85ccee40cfd9e5076ab985ab18e28351a1b86e
baksben <tatiana.bakwenye@bse.eu>
2023-11-29 13:06:35 +0100

    Changed executions in the pipeline and added pandas  to the library (modelling part)

--- spotify_library/modelling/model.py
+++ spotify_library/modelling/model.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso, Ridge
 from sklearn.metrics import mean_squared_error, r2_score

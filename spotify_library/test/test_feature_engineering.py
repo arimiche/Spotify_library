62cd85209708607ebfb697f58f3d5a01ad8d8498
arimiche <arimichelangelo@gmail.com>
2023-12-07 00:39:05 +0100

    trying to get test right

--- spotify_library/test/test_feature_engineering.py
+++ spotify_library/test/test_feature_engineering.py
@@ -1,3 +1,4 @@
+#%%
 import sys
 sys.path.append(r'C:/Users/arimi/Documents/BSE-term1/ComputingData/Final_project_Arianna_Vanessa_Tatiana')
 from spotify_library.features.feature_engineering import *
@@ -19,7 +20,7 @@ class Test_Add_Party_Music_Column(unittest.TestCase):
             'loudness': [-4, -6, -3],
             'energy': [0.6, 0.7, 0.8],
             'party_music': [1, 0, 1]})
-        pd.testing.assert_frame_equal(output, expected_result)
+        assert_frame_equal(output, expected_result)
 
     def test_novalue_add_party_music_column(self):
         example_data = {'danceability': [0.3, 0.4, 0.7],
@@ -32,7 +33,7 @@ class Test_Add_Party_Music_Column(unittest.TestCase):
             'loudness': [-4, -6, -3],
             'energy': [0.6, 0.7, 0.8],
             'party_music': [0, 0, 0]})
-        pd.testing.assert_frame_equal(output, expected_result)
+        self.assertTrue(expected_result.equals(output)) 
 
     def test_string_add_party_music_column(self):
         with self.assertRaises(TypeError):
@@ -42,7 +43,7 @@ class Test_Add_Party_Music_Column(unittest.TestCase):
             example_input = pd.DataFrame(example_data)
             add_party_music_column(example_input)
 
-
+#%%
 class Test_Add_Sleep_Music_Column(unittest.TestCase):
 
     def test_normal_add_sleep_music_column(self):
@@ -50,12 +51,12 @@ class Test_Add_Sleep_Music_Column(unittest.TestCase):
                 'duration_minutes': [6, 2, 3],
                 'energy': [0.2, 0.7, 0.8]}
         example_input = pd.DataFrame(example_data)
-        output = add_party_music_column(example_input)
+        output = add_sleep_music_column(example_input)
         expected_result = pd.DataFrame({
             'instrumentalness': [0.8, 0.3, 0.2],
             'duration_minutes': [6, 2, 3],
             'energy': [0.2, 0.7, 0.8],
-            'party_music': [1, 0, 0]})
+            'sleep_music': [1, 0, 0]})
         pd.testing.assert_frame_equal(output, expected_result)
 
     def test_empty_add_sleep_music_column(self):
@@ -63,11 +64,11 @@ class Test_Add_Sleep_Music_Column(unittest.TestCase):
                 'duration_minutes': [],
                 'energy': []}
         example_input = pd.DataFrame(example_data)
-        output = add_party_music_column(example_input)
+        output = add_sleep_music_column(example_input)
         expected_result = pd.DataFrame({
             'instrumentalness': [],
             'duration_minutes': [],
             'energy': [],
-            'party_music': []})
-        pd.testing.assert_frame_equal(output, expected_result)
+            'sleep_music': []})
+        self.assertTrue(expected_result.equals(output)) 
 

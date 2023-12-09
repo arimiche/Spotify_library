4304098bac50bfbfbf52833a486c21a9d942c8f2
Vanessa Kromm <vanessa.kromm@web.de>
2023-12-09 12:36:34 +0100

    added comments and explanations to the functions and classes

--- spotify_library/modelling/model.py
+++ spotify_library/modelling/model.py
@@ -7,24 +7,46 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 class Modeling(metaclass=ABCMeta):
     def __init__(self, name):
+        """
+        Initialize a Modeling object
+
+        Args:
+            name (str): The name of the model
+        """
         self.name = name
 
     @abstractmethod
     def trainmodel(self):
+        """
+        Abstract method for training the model
+        """
         return NotImplementedError
             
     @abstractmethod
     def predictmodel(self):
+        """
+        Abstract method for making predictions using the trained model
+        """
         return NotImplementedError
     
     @abstractmethod
     def coefficients(self):
+        """
+        Abstract method for retrieving model coefficients
+        """
         return NotImplementedError
 
 
 class LinearModel(Modeling):
 
     def __init__(self, traindata, testdata):
+        """
+        Initialize a LinearModel object
+
+        Args:
+            traindata (pd.DataFrame): The training data
+            testdata (pd.DataFrame): The testing data
+        """
         self._features = ['year', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature', 'duration_minutes', 'acoustic', 'afrobeat', 'alt-rock', 'ambient', 'black-metal', 'blues', 'breakbeat', 'cantopop', 'chicago-house', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'guitar', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'house', 'indian', 'indie-pop', 'industrial', 'jazz', 'k-pop', 'metal', 'metalcore', 'minimal-techno', 'new-age', 'opera', 'party', 'piano', 'pop', 'pop-film', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'rock', 'rock-n-roll', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'spanish', 'swedish', 'tango', 'techno', 'trance', 'trip-hop', 'mean_speechiness', 'remix', 'feat', 'live', 'love', 'my', 'vivo', 'mix', 'version', 'remastered', 'your', 'we', 'edit', 'like', 'one', 'night', 'life', 'go', 'rain', 'good', 'let', 'mixed', 'original', 'world', 'new', 'remaster', 'never', 'die']
         self._target = ['popularity']
         self.traindata = traindata
@@ -32,17 +54,29 @@ class LinearModel(Modeling):
         self._model = LinearRegression()
 
     def trainmodel(self):
+        """
+        Train the linear regression model
+        """
         X_train = self.traindata[self._features] 
         Y_train = self.traindata[self._target]
         self._model.fit(X_train, Y_train)
 
     
     def predictmodel(self):
+        """
+        Make predictions using the trained linear regression model
+
+        Returns:
+            np.ndarray: Predicted values
+        """
         X_test = self.testdata[self._features] 
         y_pred = self._model.predict(X_test)
         return y_pred
     
-    def coefficients(self):  
+    def coefficients(self):
+        """
+        Get linear regression model coefficients and display the top positive and negative coefficients
+        """  
         X_train = self.traindata[self._features] 
         feature_names = X_train.columns
         feature_values_lin = self._model.coef_
@@ -67,6 +101,13 @@ class LinearModel(Modeling):
 class LassoModel(Modeling):
 
     def __init__(self, traindata, testdata, alpha=0.5):
+        """
+        Initialize a LassoModel object
+
+        Args:
+            traindata (pd.DataFrame): The training data
+            testdata (pd.DataFrame): The testing data
+        """
         self._features = ['year', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature', 'duration_minutes', 'acoustic', 'afrobeat', 'alt-rock', 'ambient', 'black-metal', 'blues', 'breakbeat', 'cantopop', 'chicago-house', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'guitar', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'house', 'indian', 'indie-pop', 'industrial', 'jazz', 'k-pop', 'metal', 'metalcore', 'minimal-techno', 'new-age', 'opera', 'party', 'piano', 'pop', 'pop-film', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'rock', 'rock-n-roll', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'spanish', 'swedish', 'tango', 'techno', 'trance', 'trip-hop', 'mean_speechiness', 'remix', 'feat', 'live', 'love', 'my', 'vivo', 'mix', 'version', 'remastered', 'your', 'we', 'edit', 'like', 'one', 'night', 'life', 'go', 'rain', 'good', 'let', 'mixed', 'original', 'world', 'new', 'remaster', 'never', 'die']
         self._target = ['popularity']
         self.traindata = traindata
@@ -75,17 +116,29 @@ class LassoModel(Modeling):
         self._model = Lasso(alpha = self.alpha)
 
     def trainmodel(self):
+        """
+        Train the Lasso regression model
+        """
         X_train = self.traindata[self._features] 
         Y_train = self.traindata[self._target]
         self._model.fit(X_train, Y_train)
 
     
     def predictmodel(self):
+        """
+        Make predictions using the trained Lasso regression model
+
+        Returns:
+            np.ndarray: Predicted values
+        """
         X_test = self.testdata[self._features] 
         y_pred = self._model.predict(X_test)
         return y_pred
     
-    def coefficients(self):  
+    def coefficients(self):
+        """
+        Get Lasso regression model coefficients and display the top positive and negative coefficients
+        """   
         X_train = self.traindata[self._features] 
         feature_names = X_train.columns
         feature_values_lin = self._model.coef_
@@ -108,6 +161,13 @@ class LassoModel(Modeling):
 class RidgeModel(Modeling):
 
     def __init__(self, traindata, testdata, alpha=0.5):
+        """
+        Initialize a RidgeModel object
+
+        Args:
+            traindata (pd.DataFrame): The training data
+            testdata (pd.DataFrame): The testing data
+        """
         self._features = ['year', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature', 'duration_minutes', 'acoustic', 'afrobeat', 'alt-rock', 'ambient', 'black-metal', 'blues', 'breakbeat', 'cantopop', 'chicago-house', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'guitar', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'house', 'indian', 'indie-pop', 'industrial', 'jazz', 'k-pop', 'metal', 'metalcore', 'minimal-techno', 'new-age', 'opera', 'party', 'piano', 'pop', 'pop-film', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'rock', 'rock-n-roll', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'spanish', 'swedish', 'tango', 'techno', 'trance', 'trip-hop', 'mean_speechiness', 'remix', 'feat', 'live', 'love', 'my', 'vivo', 'mix', 'version', 'remastered', 'your', 'we', 'edit', 'like', 'one', 'night', 'life', 'go', 'rain', 'good', 'let', 'mixed', 'original', 'world', 'new', 'remaster', 'never', 'die']
         self._target = ['popularity']
         self.traindata = traindata
@@ -116,17 +176,29 @@ class RidgeModel(Modeling):
         self._model = Ridge(alpha = self.alpha)
 
     def trainmodel(self):
+        """
+        Train the Ridge regression model
+        """
         X_train = self.traindata[self._features] 
         Y_train = self.traindata[self._target]
         self._model.fit(X_train, Y_train)
 
     
     def predictmodel(self):
+        """
+        Make predictions using the trained Ridge regression model
+
+        Returns:
+            np.ndarray: Predicted values
+        """
         X_test = self.testdata[self._features] 
         y_pred = self._model.predict(X_test)
         return y_pred
     
     def coefficients(self):  
+        """
+        Get Ridge regression model coefficients and display the top positive and negative coefficients
+        """ 
         X_train = self.traindata[self._features] 
         feature_names = X_train.columns
         feature_values_lin = self._model.coef_
@@ -147,6 +219,13 @@ class RidgeModel(Modeling):
         print(Ridge_coeff.tail(10)[::-1])  
     
 def model_performance(y_true, y_pred):
+    """
+    Calculate and display model performance metrics: MSE and R-squared
+
+    Args:
+        y_true (np.ndarray): True values
+        y_pred (np.ndarray): Predicted values    
+    """
     # Calculate Mean Squared Error (MSE)
     mse = mean_squared_error(y_true, y_pred)
     print("Mean Squared Error (MSE):", mse)
@@ -157,6 +236,16 @@ def model_performance(y_true, y_pred):
 
 
 def hyperparameter_tuning(prediction_model, X_train, y_train, X_test, y_test):
+    """
+    Tune hyperparameters of the given prediction model using alpha values
+
+    Args:
+        prediction_model: The prediction model class
+        X_train (pd.DataFrame): Training features
+        y_train (np.ndarray): Training target
+        X_test (pd.DataFrame): Testing features
+        y_test (np.ndarray): Testing target    
+    """
 
     # Define a range of alpha values to try
     alphas = np.linspace(0.001, 10, 200)
